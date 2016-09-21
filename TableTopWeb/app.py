49e8ab2aba66852fa28fea0eef750b936e738281
Shaun Chiang <redbeansodapop@hotmail.com>
2016-09-22 00:22:34 +0800

    Scoreboard 'main menu' barebones.

--- TableTopWeb/app.py
+++ TableTopWeb/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, flash
+
+from flask import Flask, render_template, flash,request, redirect, url_for
 from flask_bootstrap import Bootstrap
+
 from wtforms import StringField, SubmitField
 from flask_wtf import Form
 import random
 
 app = Flask(__name__)
+scoreboard_dict = dict()
+current_room_id = 0
 
 class scoreboard_form(Form):
     player_names_field = StringField('Player Names:', description='Input your player names seperated by commas. (e.g. Shaun, Ryan, Junsheng)')
@@ -15,22 +19,33 @@ def index():
     # TODO: Homepage & dice roller
     return render_template('index.html')
 
-@app.route('/scoreboard')
+@app.route('/scoreboard', methods=['GET', 'POST'])
 def scoreboard():
+    global current_room_id
     form = scoreboard_form()
+    if request.method == 'POST' and form.validate():
+        input = form.player_names_field.data
+        list_of_names = input.split(",")
+        for i in xrange(len(list_of_names)):
+            list_of_names[i] = list_of_names[i].strip()
+            current_room_id+=1
+            scoreboard_dict[current_room_id]=dict()
+            scoreboard_dict[current_room_id][list_of_names[i]] = 0
+        return redirect(url_for('scoreboardid', id=current_room_id))
+
     return render_template('scoreboard_home.html', form=form)
 
 @app.route('/scoreboard/<id>')
 def scoreboardid(id):
+    global current_room_id
     try:
         if (scoreboard_dict[int(id)]):
             return render_template('scoreboard.html', id=id)
         else:
-            room_id = random.randint(0,10000)
-            scoreboard_dict[room_id] = dict()
-            return render_template('scoreboard.html', id=room_id)
+            current_room_id += 1
+            scoreboard_dict[current_room_id] = dict()
+            return render_template('scoreboard.html', id=current_room_id)
     except:
-        flash('Invalid Room Id', 'error')
         return render_template('invalid_scoreboard.html')
 
 
@@ -45,7 +60,6 @@ def dice_rollers():
     return render_template('dice.html')
 
 if __name__ == '__main__':
-    scoreboard_dict = dict()
     Bootstrap(app)
     app.secret_key = 'devkey'
     app.config['SESSION_TYPE'] = 'filesystem'
