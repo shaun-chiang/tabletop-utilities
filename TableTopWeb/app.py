f008460b41813d8cec3efaec2ec8736fe15c3b6f
Shaun Chiang <redbeansodapop@hotmail.com>
2016-09-25 18:04:36 +0800

    Basic Scoreboard functionality done.

--- TableTopWeb/app.py
+++ TableTopWeb/app.py
@@ -2,7 +2,8 @@
 # Description: Main app for TableTopWeb, a page holding common utilities for tabletop gaming.
 #              Please install requirements in requirements.txt first.
 import sys
-from flask import Flask, render_template, flash,request, redirect, url_for
+from collections import OrderedDict
+from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View, Text
@@ -14,14 +15,18 @@ app = Flask(__name__)
 scoreboard_dict = dict()
 current_room_id = 0
 
+
 class scoreboard_form(Form):
-    player_names_field = StringField('Player Names:', [validators.DataRequired()], description='Input your player names seperated by commas. (e.g. Shaun, Ryan, Junsheng)')
+    player_names_field = StringField('Player Names:', [validators.DataRequired()],
+                                     description='Input your player names seperated by commas. (e.g. Shaun, Ryan, Junsheng)')
     submit_button = SubmitField("Let's Play!!")
+
 
 @app.route('/')
 def index():
     # TODO: Homepage & dice roller
     return render_template('index.html')
+
 
 @app.route('/scoreboard', methods=['GET', 'POST'])
 def scoreboard():
@@ -30,14 +35,15 @@ def scoreboard():
     if request.method == 'POST' and form.validate():
         input = form.player_names_field.data
         list_of_names = input.split(",")
-        current_room_id+=1
-        scoreboard_dict[current_room_id]={}
+        current_room_id += 1
+        scoreboard_dict[current_room_id] = OrderedDict()
         for i in range(len(list_of_names)):
             list_of_names[i] = list_of_names[i].strip()
             scoreboard_dict[current_room_id][list_of_names[i]] = 0
         print(scoreboard_dict)
         return redirect(url_for('scoreboardid', id=current_room_id))
     return render_template('scoreboard_home.html', form=form)
+
 
 @app.route('/scoreboard/<id>')
 def scoreboardid(id):
@@ -52,19 +58,38 @@ def scoreboardid(id):
         return render_template('invalid_scoreboard.html')
 
 
+@app.route('/_increment_number')
+def increment_number():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    scoreboard_dict[b][list(scoreboard_dict[b].items())[a-1][0]] += 1
+    print(scoreboard_dict)
+    return jsonify(result=scoreboard_dict[b][list(scoreboard_dict[b].items())[a-1][0]])
+
+@app.route('/_decrement_number')
+def decrement_number():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    scoreboard_dict[b][list(scoreboard_dict[b].items())[a - 1][0]] -= 1
+    print(scoreboard_dict)
+    return jsonify(result=scoreboard_dict[b][list(scoreboard_dict[b].items())[a - 1][0]])
+
+
 @app.route('/trending_games')
 def trending_games():
-    #TODO: trending_games
+    # TODO: trending_games
     bgg = BoardGameGeek()
     hot_items_dict = dict()
     for item in bgg.hot_items("boardgame"):
         hot_items_dict[item.id] = item.name
-    return render_template('page.html', hot_items_dict = hot_items_dict)
+    return render_template('page.html', hot_items_dict=hot_items_dict)
+
 
 @app.route('/dice_roller')
 def dice_rollers():
-    #TODO: dice_roller
+    # TODO: dice_roller
     return render_template('dice.html')
+
 
 if __name__ == '__main__':
     Bootstrap(app)
@@ -75,10 +100,10 @@ if __name__ == '__main__':
         '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
     nav = Nav()
     nav.register_element('top', Navbar(
-    View('TableTopWeb', '.index'),
-    View('Dice Roller', '.dice_rollers'),
-    View('Scoreboard', '.scoreboard'),
-    View('Trending Games', '.trending_games'),
-    Text('Networks Lab 2 by Junsheng & Shaun'),))
+        View('TableTopWeb', '.index'),
+        View('Dice Roller', '.dice_rollers'),
+        View('Scoreboard', '.scoreboard'),
+        View('Trending Games', '.trending_games'),
+        Text('Networks Lab 2 by Junsheng & Shaun'), ))
     nav.init_app(app)
     app.run(debug=True, host='0.0.0.0')
